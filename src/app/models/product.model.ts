7abaae5888552e9a8a67e7aa267831b75fa86b2a
Laércio Oliveira <laercio@ar.quei.ro>
2020-11-12 21:06:21 -0300

    feat: create models

---
+++ src/app/models/product.model.ts
@@ -0,0 +1,9 @@
+export class ProductModel {
+    // tslint:disable-next-line: variable-name
+    public _id: string;
+    public title: string;
+    public category: string;
+    public description: string;
+    public price: string;
+    public images: string[];
+}
